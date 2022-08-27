c1b70367b43de9a06c3b5a6ffd5ccb9432a8dd78
Aldiii <111747491+ItsMeALdii@users.noreply.github.com>
2022-08-27 14:23:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,16 +16,16 @@ global.xteam = 'apivproject'
 global.lolhuman = 'yourkey'
 
 // Other
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.ownername = 'YahyaGanzz'
-global.botname = 'AzzBot•MD'
-global.packname = '© YahyaGanzz'
-global.gc = 'https://github.com/AzzBott679'
-global.linkyt = 'https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g'
-global.linkgc = 'https://chat.whatsapp.com/CkguID8hSdSFAaBrKNHLSr'
+global.owner = ['62895331245483']
+global.premium = ['62895331245483']
+global.ownername = '𝙞𝙩𝙨𝙈𝙚𝘼𝙇𝙙𝙞𝙞 ★'
+global.botname = 'ALdiixBotZ'
+global.packname = '© 𝘼𝙇𝙙𝙞𝙞 𝙞𝙨 𝙃𝙚𝙧𝙚 ★'
+global.gc = 'https://github.com/ItsMeALdii'
+global.linkyt = 'https://youtube.com/channel/UCKHu6PJcxYcbWBiBV0UPOlA'
+global.linkgc = 'https://chat.whatsapp.com/Fq0TKkkm8iMKgdcQic2aX3'
 global.limitawal = '10'
-global.author = '@yahyaganzz\n@AzzBot'
+global.author = '@req_aldiee\n@foxxBot'
 global.sessionName = 'sessionya'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⌕'
@@ -33,7 +33,7 @@ global.mess = {
     success: 'Berhasil',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
+    owner: 'Fitur Khusus 𝙞𝙩𝙨𝙈𝙚𝘼𝙇𝙙𝙞𝙞 ★',
     group: 'Fitur Digunakan Hanya Untuk Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
@@ -48,7 +48,7 @@ global.sc = fs.readFileSync('./media/sc.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
 global.menu = fs.readFileSync('./media/menu.jpg')
 global.allmenu = fs.readFileSync('./media/allmenu.jpg')
-global.thumb = { url: 'https://i.pinimg.com/736x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg' }
+global.thumb = { url: 'https://telegra.ph/file/9967164c85976f551bb05.jpg' }
 global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
 
 let file = require.resolve(__filename)
